37ace6f06257b27c91b4939c104a18332b5b0718
Payoja <payoja.muduli@gmail.com>
2022-02-18 19:20:28 +0530

    added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
 import { Typography } from '@material-ui/core';
-import { useLocation} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import  grapesjs  from 'grapesjs';
+import gjsPresetWebPage from 'grapesjs-preset-webpage'
+import '../../node_modules/grapesjs/dist/css/grapes.min.css';
+import '../../node_modules/grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css';
+import '../styles/main.scss'
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 
 const Home =()=>{
-    const location = useLocation();
-    const html  = location.state.params;
-    const css  = location.state.anotherparams;
+    const [editor, setEditor] = useState(null);
+    const history = useHistory();
+    const [html, setHtml] = useState(null);
+    const [css, setCss] = useState(null);
+    useEffect(()=>{
+        const editor = grapesjs.init({
+          container:"#editor",
+          plugins:[gjsPresetWebPage, 'gjs-blocks-basic'],
+          pluginsOpts:{
+            gjsPresetWebPage:{}
+          },
+        });
+        setHtml(editor.getHtml());
+        setCss(editor.getCss())
+        setEditor(editor);
+      }, []);
     return(
         <div style={{display:"flex", flexDirection:"column"}}>
            <div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,10 +7,15 @@ const Welcome =()=>{
     const changePage =()=>{
         history.push('/grapesjs')
     }
+    const codePage =()=>{
+        history.push('/home')
+    }
     return(
         <div style={{textAlign: 'center', marginTop:300}}>
         <Typography variant="h4">Click here to go to the Editor</Typography>
           <Button variant="contained" color="secondary" onClick={changePage}>Design</Button>
+          <Typography variant="h4">Click here to go to the Code Page</Typography>
+          <Button variant="contained" color="primary" onClick={codePage}>Code</Button>
         </div>
     )
 }
